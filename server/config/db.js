588e7c5e02705ea906f8c6d7854f267a9f514627
RasheedAdeyemi <radeyem2@my.centennialcollege.ca>
2023-03-04 18:53:44 -0500

    initial commit

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+//Rasheed Adeyemi 301308878 04/03/2023
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb+srv://engrwhite:Prince018@cluster0.pcpzxes.mongodb.net/?retryWrites=true&w=majority"
+  "URI": "mongodb+srv://engrwhite18:TQNn2PtQMICz9wRV@cluster0.bilwzkp.mongodb.net/?retryWrites=true&w=majority"
+};
